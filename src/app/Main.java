09f97610af966fdcb7799d1ca0b67e387399be24
Ed Gluschenko <edgluschenko3@gmail.com>
2025-01-31 16:20:36 +0200

    add message output

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,14 +1,15 @@
 package app;
 
 public class Main {
+
     private static final double CONV_K=2.20462;
 
     public static void main(String[] args) {
         System.out.println("Converter app.");
+        System.out.println("App for measures converting");
         double kgs=5;
       double pounds = convKgsToPounds(kgs);
         System.out.println("Result is "+ pounds+" pounds.");
-
     }
 
     private static double convKgsToPounds(double kgs){
